70366e863ca477e240fc95154954200658f7c0ea
kucseranna <88683177+kucseranna@users.noreply.github.com>
2022-02-08 17:07:32 +0100

    dashboard+routing temp

--- barbieshop/src/app/app-routing.module.ts
+++ barbieshop/src/app/app-routing.module.ts
@@ -1,7 +1,77 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'product',
+    component: DashboardComponent,
+  },
+  {
+    path: 'customer',
+    component: DashboardComponent,
+  },
+  {
+    path: 'order',
+    component: DashboardComponent,
+  },
+  {
+    path: 'bill',
+    component: DashboardComponent,
+  },
+  {
+    path: 'product-list',
+    component: DashboardComponent,
+  },
+  {
+    path: 'customer-list',
+    component: DashboardComponent,
+  },
+  {
+    path: 'order-list',
+    component: DashboardComponent,
+  },
+  {
+    path: 'bill-list',
+    component: DashboardComponent,
+  },
+  {
+    path: 'edit-product',
+    component: DashboardComponent,
+  },
+  {
+    path: 'edit-customer',
+    component: DashboardComponent,
+  },
+  {
+    path: 'edit-order',
+    component: DashboardComponent,
+  },
+  {
+    path: 'add-product',
+    component:  DashboardComponent,
+  },
+  {
+    path: 'add-customer',
+    component:  DashboardComponent,
+  },
+  {
+    path: 'add-order',
+    component:  DashboardComponent,
+  },
+  {
+    path: 'add-bill',
+    component:  DashboardComponent,
+  },
+    {
+    path: '**',
+    component:  DashboardComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- barbieshop/src/app/app.module.ts
+++ barbieshop/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
 import { HeaderComponent } from './common/header/header.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
