adedcba8f0f2ba2d9dde0b62e57bc8cb892d0a4e
zsofip <91950899+zsofip@users.noreply.github.com>
2022-02-08 22:21:07 +0100

    product service added

--- barbieshop/src/app/service/product.service.ts
+++ barbieshop/src/app/service/product.service.ts
@@ -1,5 +1,8 @@
-import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +10,33 @@ import { Injectable } from '@angular/core';
 export class ProductService {
   apiUrl: string = environment.apiUrl;
 
-  constructor() { }
+  entityName: string = 'product';
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiUrl}${this.entityName}`);
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}${this.entityName}/${id}`);
+  }
+
+  addProduct(product: Product): Observable<any> {
+    return this.http.post<Observable<any>>(`${this.apiUrl}${this.entityName}`,
+    product);
+  }
+
+  updateProduct(product: Product): Observable<Product> {
+    return this.http.patch<Product>(
+      `${this.apiUrl}${this.entityName}/${product.id}`,
+      product
+    );
+  }
+
+  removeProduct(product: Product): Observable<any> {
+    return this.http.delete(`${this.apiUrl}${this.entityName}/${product.id}`);
+  }
 }
