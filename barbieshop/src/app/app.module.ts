bad51b3d73dee033fcbdcdd537908c62ba9cd912
zsofip <91950899+zsofip@users.noreply.github.com>
2022-02-08 23:33:57 +0100

    product list c first version

--- barbieshop/src/app/app.module.ts
+++ barbieshop/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
 import { HeaderComponent } from './common/header/header.component';
 import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { ProductListComponent } from './common/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
