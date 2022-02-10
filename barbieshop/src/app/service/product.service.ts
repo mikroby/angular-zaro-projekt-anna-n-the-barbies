0c5eee0ccf5c8775ec88cdf0e4a20227b5d18ed8
zsofip <91950899+zsofip@users.noreply.github.com>
2022-02-10 21:47:16 +0100

    product service fixed, custom-currency pipe added

--- barbieshop/src/app/service/product.service.ts
+++ barbieshop/src/app/service/product.service.ts
@@ -3,46 +3,50 @@ import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../model/product';
+import { BaseService } from './base.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class ProductService extends BaseService<Product> {
 
   // apiUrl: string = "http://localhost:3000/"
-  apiUrl: string = environment.apiUrl;
-
-  entityName: string = 'product';
+  // apiUrl: string = environment.apiUrl;
 
   constructor(
-    private http: HttpClient,
-  ) { }
-
-  getAllProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(`${this.apiUrl}${this.entityName}`);
+    public override http: HttpClient,
+  ) {
+    super(http);
+    this.entityName = 'product';
   }
 
-  getProductById(id: number): Observable<Product> {
-    return this.http.get<Product>(`${this.apiUrl}${this.entityName}/${id}`);
-  }
+}
 
-  addProduct(product: Product): Observable<any> {
-    return this.http.post<Observable<any>>(`${this.apiUrl}${this.entityName}`,
-    product);
-  }
+  // getAllProducts(): Observable<Product[]> {
+  //   return this.http.get<Product[]>(`${this.apiUrl}${this.entityName}`);
+  // }
 
-  updateProduct(product: Product): Observable<Product> {
-    return this.http.patch<Product>(
-      `${this.apiUrl}${this.entityName}/${product.id}`,
-      product
-    );
-  }
+  // getProductById(id: number): Observable<Product> {
+  //   return this.http.get<Product>(`${this.apiUrl}${this.entityName}/${id}`);
+  // }
+
+  // addProduct(product: Product): Observable<any> {
+  //   return this.http.post<Observable<any>>(`${this.apiUrl}${this.entityName}`,
+  //   product);
+  // }
+
+  // updateProduct(product: Product): Observable<Product> {
+  //   return this.http.patch<Product>(
+  //     `${this.apiUrl}${this.entityName}/${product.id}`,
+  //     product
+  //   );
+  // }
 
   // removeProduct(product: Product): Observable<any> {
   //   return this.http.delete(`${this.apiUrl}${this.entityName}/${product.id}`);
   // }
 
-  removeProduct(id: number): Observable<Product> {
-    return this.http.delete<Product>(`${this.apiUrl}${this.entityName}/${id}`);
-  }
-}
+//   removeProduct(id: number): Observable<Product> {
+//     return this.http.delete<Product>(`${this.apiUrl}${this.entityName}/${id}`);
+//   }
+
