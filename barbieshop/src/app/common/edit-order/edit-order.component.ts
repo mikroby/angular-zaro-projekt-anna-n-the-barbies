93974ed07db40282a57e50b653b19b71cae517b3
mikroby <rtamas@hotmail.com>
2022-02-11 17:22:34 +0100

    edit-order reacts to id

--- barbieshop/src/app/common/edit-order/edit-order.component.ts
+++ barbieshop/src/app/common/edit-order/edit-order.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { Order } from 'src/app/model/order';
+import { OrderService } from 'src/app/service/order.service';
 
 @Component({
   selector: 'app-edit-order',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOrderComponent implements OnInit {
 
-  constructor() { }
+  order!: Order;
+
+  newOrder: Order = new Order();
+
+  id!: string;
+
+
+  constructor(
+    private orderService: OrderService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
 
   ngOnInit(): void {
+
+    this.activatedRoute.params.pipe(
+      switchMap(params => this.id = params['id'])).subscribe(
+        () => {
+          if (this.id === '0') {
+            this.order = this.newOrder;
+          } else {
+            this.orderService.getOne(Number(this.id)).subscribe(
+              (result) => this.order = result
+            )
+          }
+        }
+        ,
+      );
   }
 
+  onUpdateorder(order: Order): void {
+
+  }
+
+  onRemoveorder(order: Order): void {
+
+  }
 }
