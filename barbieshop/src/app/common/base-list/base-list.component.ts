346ac1b77ff6b9057321cae7ebf579b1b64559a1
mikroby <rtamas@hotmail.com>
2022-02-21 16:18:53 +0100

    isNestedObject method added

--- barbieshop/src/app/common/base-list/base-list.component.ts
+++ barbieshop/src/app/common/base-list/base-list.component.ts
@@ -3,7 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Component, EventEmitter, Input, OnInit, Output, AfterViewInit, ViewChild } from '@angular/core';
-import { flatMap, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-base-list',
@@ -95,5 +95,8 @@ export class BaseListComponent implements OnInit, AfterViewInit {
   isBoolean(value: any): boolean {
     return (typeof value === 'boolean');
   }
-
+  
+  isNestedObject(value: any): boolean {
+    return (typeof value === 'object');
+  }
 }
